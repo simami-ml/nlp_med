4174b3dce88bc7a02c6faecee1866565df274e84
Mary <101702652+simami-ml@users.noreply.github.com>
2024-05-20 23:25:20 +0300

    Update dataload.py

--- scr/dataload.py
+++ scr/dataload.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def dataload():
-    df = pd.read_csv(f'../data/train_df_processed.csv', index_col=[0])
+    df = pd.read_csv(f'/mount/src/nlp_med/data/train_df_processed.csv', index_col=[0])
     questions = pd.concat([df['question_1'],df['question_2']],axis=0).drop_duplicates(keep='first')
     return questions
 
